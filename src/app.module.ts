7925525d3ecf24ea907f8ff4e5e3ef87703891f7
Rayper <raihangunawan22@gmail.com>
2022-03-02 15:35:10 +0700

    video 50 Asynchronously Configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,23 +19,27 @@ import appConfig from './config/app.config';
     //     DATABASE_PORT: Joi.number().default(5432),
     //   })
     // }),
+    // forRootAsync memungkinkan untuk TypeOrmModule dijalankan setelah seluruh module selesai dijalankan
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        // + supaya dibaca angka
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        // memastikan bahwa entities akan sync dengan database setiap kita run apps nya
+        // ketika di production, set to disabled
+        synchronize: true
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig]
     }),
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      // + supaya dibaca angka
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      // memastikan bahwa entities akan sync dengan database setiap kita run apps nya
-      // ketika di production, set to disabled
-      synchronize: true
-    }),
+    CoffeesModule, 
+    
     CoffeeRatingModule,
     DatabaseModule,
   ],
